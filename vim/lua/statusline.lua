51e971fa9d04a4524ba55b85dc25fb535011842b
El Kapuzo <el.kapuzo@mailbox.org>
2022-01-07 12:41:07 +0100

    feat(vim): add statusline in lua

---
+++ vim/lua/statusline.lua
@@ -0,0 +1,124 @@
+local seperator = '│'
+
+local mode_display = {
+    n = "NORMAL",
+    no = "Nop",
+    nov = "Nop",
+    noV = "Nop",
+    ["noCTRL-V"] = "Nop",
+    niI = "NORMAL",
+    niR = "NORMAL",
+    niV = "NORMAL",
+    v = "VISUAL",
+    V = "V-LINE",
+    ["ctrl-v"] = "V-BLOCK",
+    s = "SELECT",
+    S = "S-LINE", ["ctrl-s"] = "S-BLOCK",
+    i = "INSERT",
+    ic = "INSERT",
+    ix = "INSERT",
+    r = "REPLACE",
+    R = "REPLACE",
+    rc = "REPLACE",
+    rv = "REPLACE",
+    rx = "REPLACE",
+    c = "COMMAND",
+    cv = "COMMAND",
+    ce = "COMMAND",
+    r = "PROMPT",
+    rm = "PROMPT",
+    ["r?"] = "PROMPT",
+    ["!"] = "SHELL",
+    t = "TERM",
+    ["null"] = "NONE"
+}
+local mode_hl_group = {
+    n = "Normal",
+    no = "Normal",
+    nov = "Normal",
+    noV = "Normal",
+    ["noCTRL-V"] = "Normal",
+    niI = "Normal",
+    niR = "Normal",
+    niV = "Normal",
+    v = "Visual",
+    V = "Visual",
+    ["ctrl-v"] = "Visual",
+    s = "Select",
+    S = "Select",
+    ["ctrl-s"] = "Select",
+    i = "Insert",
+    ic = "Insert",
+    ix = "Insert",
+    r = "Replace",
+    R = "Replace",
+    rc = "Replace",
+    rv = "Replace",
+    rx = "Replace",
+    c = "Command",
+    cv = "Command",
+    ce = "Command",
+    r = "Prompt",
+    rm = "Prompt",
+    ["r?"] = "Prompt",
+    ["!"] = "Shell",
+    t = "Term",
+    ["null"] = "None"
+}
+
+function mode()
+    local hl_group = "StatusLine" .. mode_hl_group[vim.fn.mode()]
+    vim.cmd("hi link StatusLineMode " .. hl_group)
+    return mode_display[vim.fn.mode()]
+end
+
+local function git()
+    return "%( %{luaeval('git_branch()')} %)"
+end
+
+function git_branch()
+    local branch = vim.fn["fugitive#head"]()
+    return branch
+end
+
+function status_line_active()
+    return table.concat{
+        "%#StatusLineMode#",
+        "%( %{luaeval('mode()')} %)",
+        "%#StatusLineLeft#",
+        git(),
+        "%#StatusLineMiddle#",
+        "%( %t %)",
+        seperator,
+        "%( %m %)",
+        "%=",
+        "%( %{&fileformat} %)",
+        seperator,
+        "%( %{&fileencoding?&fenc:&enc} %)",
+        seperator,
+        '%( %y %)',
+        '%#StatusLineRight#',
+        '%( %p%% %)',
+        '%#StatusLineFarRight#',
+        '%( %3l:%-2c %)'
+    }
+end
+
+function status_line_inactive() 
+    return table.concat{
+        "%#StatusLineInactive#",
+        "%( %t %)",
+        "%=",
+        '%( %p%% %)',
+        '%( %3l:%-2c %)'
+    }
+end
+
+vim.cmd [[
+   augroup statusline
+     au!
+     au winenter,bufenter * setlocal statusline=%!v:lua.status_line_active()
+     au winleave,bufleave * setlocal statusline=%!v:lua.status_line_inactive()
+     augroup end
+]]
+
