27a07c37813c8b36b5bd7b898a69dfa578d8c533
El Kapuzo <el.kapuzo@mailbox.org>
2022-01-11 22:51:00 +0100

    feat(shell): update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,3 +1,11 @@
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add .'
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gc='git commit'
+alias gcamend='git commit --amend --no-edit'
+
 alias ls='ls --color'
 alias la='ls -la --color'
 
