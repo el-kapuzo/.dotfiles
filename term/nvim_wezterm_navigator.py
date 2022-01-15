960de6690aa042c102793312aaa46cc2147489a4
El Kapuzo <el.kapuzo@mailbox.org>
2022-01-15 22:07:51 +0100

    feat(wezterm): add resizing to wezterm navigator

--- term/nvim_wezterm_navigator.py
+++ term/nvim_wezterm_navigator.py
@@ -13,12 +13,47 @@ directions = {
 opts = argparse.ArgumentParser()
 opts.add_argument("--socket")
 opts.add_argument("--direction")
+opts.add_argument("action")
 
-args = opts.parse_args()
 
-nvim = pynvim.attach("socket", path=args.socket)
-winnr = nvim.call("winnr")
-nvim.command(f"wincmd {directions.get(args.direction, args.direction)}")
+def main(args):
+    nvim = connect(args.socket)
+    if args.action == "move":
+        success = maybe_move(nvim, args.direction)
+    elif args.action == "resize":
+        success = maybe_resize(nvim, args.direction)
+    else:
+        print("Something went wrong")
+        sys.exit(3)
+    if not success:
+        sys.exit(1)
 
-if winnr == nvim.call("winnr"):
-    sys.exit(1)
+
+def connect(path):
+    try:
+        return pynvim.attach("socket", path=path)
+    except Exception:
+        sys.exit(2)
+
+
+def maybe_resize(nvim, direction):
+    commands = {
+        "Up": "resize +3",
+        "Down": "resize -3",
+        "Left": "vertical resize -3",
+        "Right": "vertical resize +3",
+    }
+    nvim.command(commands[direction])
+    print(f"Resized in direction: {direction}")
+    return True
+
+
+def maybe_move(nvim, direction):
+    winnr = nvim.call("winnr")
+    nvim.command(f"wincmd {directions.get(direction, direction)}")
+
+    return not (winnr == nvim.call("winnr"))
+
+
+if __name__ == "__main__":
+    main(opts.parse_args())
--- term/wezterm.lua
+++ term/wezterm.lua
@@ -1,23 +1,43 @@
-local wezterm = require 'wezterm';
+local wezterm = require 'wezterm'
 local os = require'os'
 
-function send_to_nvim(window, pane, direction) 
+function run_nvim_navigator(pane_id, nvim_action, direction)
     local dofile_path = os.getenv("HOME") .. "/.dotfiles"
     local py_path = dofile_path .. "/.venv/bin/python"
     local py_script_path = dofile_path .. "/term/nvim_wezterm_navigator.py"
-    local nvim_address = "/tmp/nvim_wezterm_" .. pane:pane_id()
-    local result = os.execute(py_path .. " " .. py_script_path .. " --socket " .. nvim_address .. " --direction " .. direction)
+    local nvim_address = "/tmp/nvim_wezterm_" .. pane_id
+    return os.execute(py_path .. " " .. py_script_path .. " --socket " .. nvim_address .. " --direction " .. direction .. " " .. nvim_action)
+end
+
+function move(window, pane, direction) 
+    local result = run_nvim_navigator(pane:pane_id(), "move", direction)
     if result then
         wezterm.log_info("succesfully changed nvim window")
     else
+        wezterm.log_info("changing wezterm window")
         window:perform_action(wezterm.action({ActivatePaneDirection = direction}), pane)
     end
 end
 
-wezterm.on("nvim-left", function(window, pane) send_to_nvim(window, pane, "Left") end)
-wezterm.on("nvim-right", function(window, pane) send_to_nvim(window, pane, "Right") end)
-wezterm.on("nvim-down", function(window, pane) send_to_nvim(window, pane, "Down") end)
-wezterm.on("nvim-up", function(window, pane) send_to_nvim(window, pane, "Up") end)
+function resize(window, pane, direction)
+    local result = run_nvim_navigator(pane:pane_id(), "resize", direction)
+    if result then
+        wezterm.log_info("succesfully resized nvim window")
+    else
+        wezterm.log_info("resizing wezterm window")
+        window:perform_action(wezterm.action({ AdjustPaneSize = { direction, 3 } }), pane)
+    end
+end
+
+wezterm.on("move-left", function(window, pane) move(window, pane, "Left") end)
+wezterm.on("move-right", function(window, pane) move(window, pane, "Right") end)
+wezterm.on("move-down", function(window, pane) move(window, pane, "Down") end)
+wezterm.on("move-up", function(window, pane) move(window, pane, "Up") end)
+
+wezterm.on("resize-left", function(window, pane) resize(window, pane, "Left") end)
+wezterm.on("resize-right", function(window, pane) resize(window, pane, "Right") end)
+wezterm.on("resize-down", function(window, pane) resize(window, pane, "Down") end)
+wezterm.on("resize-up", function(window, pane) resize(window, pane, "Up") end)
 
 return {
     font_size = 10.5,
@@ -48,22 +68,29 @@ return {
         -- PANES ------------------------------
         {key="s", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
         {key="v", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        {key="d", mods="LEADER", action=wezterm.action{CloseCurrentPane={confirm=false}}},
+        {key="z", mods="LEADER", action="TogglePaneZoomState"},
+
         {key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
         {key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
         {key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
         {key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
-        -- NVIM AWARE NAVIGATION ----------------------
-        {key="h", mods="CTRL", action=wezterm.action{EmitEvent = "nvim-left"}},
-        {key="j", mods="CTRL", action=wezterm.action{EmitEvent="nvim-down"}},
-        {key="k", mods="CTRL", action=wezterm.action{EmitEvent="nvim-up"}},
-        {key="l", mods="CTRL", action=wezterm.action{EmitEvent="nvim-right"}},
 
         {key="h", mods="ALT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
         {key="j", mods="ALT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
         {key="k", mods="ALT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
         {key="l", mods="ALT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-        {key="d", mods="LEADER", action=wezterm.action{CloseCurrentPane={confirm=false}}},
-        {key="z", mods="LEADER", action="TogglePaneZoomState"},
+
+        -- NVIM AWARE NAVIGATION ----------------------
+        {key="h", mods="CTRL", action=wezterm.action{EmitEvent = "move-left"}},
+        {key="j", mods="CTRL", action=wezterm.action{EmitEvent="move-down"}},
+        {key="k", mods="CTRL", action=wezterm.action{EmitEvent="move-up"}},
+        {key="l", mods="CTRL", action=wezterm.action{EmitEvent="move-right"}},
+
+        {key="LeftArrow", mods="ALT", action=wezterm.action{EmitEvent = "resize-left"}},
+        {key="DownArrow", mods="ALT", action=wezterm.action{EmitEvent = "resize-down"}},
+        {key="UpArrow", mods="ALT", action=wezterm.action{EmitEvent = "resize-up"}},
+        {key="RightArrow", mods="ALT", action=wezterm.action{EmitEvent = "resize-right"}},
 
         -- APPLICATION
         {key="q", mods="LEADER", action="QuitApplication"},
