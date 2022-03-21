4d2a4521ae963e35322279f5d81e1034e6a628c0
El Kapuzo <el.kapuzo@mailbox.org>
2022-03-21 20:28:23 +0100

    fix(shell): rm sourcing of deleted file

--- shell/env.sh
+++ shell/env.sh
@@ -1,12 +1,10 @@
 export EDITOR="nvim"
 export MANPAGER='nvim +Man!'
+
 export PASSWORD_STORE_GENERATED_LENGTH=32
 
 export FZF_DEFAULT_COMMAND='rg --files'
 export SKIM_DEFAULT_COMMAND='rg --files'
-
-export BAT_CONFIG_PATH=$DOTFILES/bat/bat.config
-export RIPGREP_CONFIG_PATH=$DOTFILES/ripgrep/rg.config
 
 # ------------ PYTHON ---------------
 if ! [[ $(uname -s) == MINGW* ]];
