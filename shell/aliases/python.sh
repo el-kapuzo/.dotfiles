f68640383c6faa6695efb9ab84164b2f6f3f7fae
Daniel Pohl <daniel.pohl@up2parts.com>
2022-06-10 12:51:46 +0200

    refactor(shell): move more shell functions to scripts

--- shell/aliases/python.sh
+++ shell/aliases/python.sh
@@ -6,39 +6,3 @@ else
     alias flake8="$DOTFILES/.venv/bin/flake8"
 fi
 
-
-pyenv () {
-    if [ -d "./.venv" ];
-    then
-        __activate_env
-    else
-        py -m venv ./.venv
-        if [ -d "./.venv/bin/" ];
-        then
-            ./.venv/bin/python -m pip install --upgrade pip setuptools
-        else
-            ./.venv/Scripts/python.exe -m pip install --upgrade pip setuptools
-        fi
-        __activate_env
-    fi
-}
-
-rmpyenv () {
-    if [ -d "./.venv" ];
-    then
-        if [ "${VIRTUAL_ENV+x}" = "x" ];
-        then
-            deactivate
-        fi
-        rm -r ./.venv
-    fi
-}
-
-__activate_env () {
-    if [ -d "./.venv/bin/" ];
-    then
-        . ./.venv/bin/activate
-    else
-        . ./.venv/Scripts/activate
-    fi
-}
