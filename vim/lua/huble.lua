14da3d8501e8e066747eecb7040ca728c4b3e8ab
El Kapuzo <el.kapuzo@mailbox.org>
2023-05-02 21:03:44 +0200

    feat(vim): add keymap for changing colorschemes

--- vim/lua/huble.lua
+++ vim/lua/huble.lua
@@ -1,10 +1,12 @@
-packadd = require'utils'.packadd
+local packadd = require'utils'.packadd
 
 packadd("plenary.nvim")
 packadd("telescope.nvim")
 
-builtin_pickers = require'telescope.builtin'
+local builtin_pickers = require'telescope.builtin'
 
 vim.keymap.set('n', '<leader>ff', builtin_pickers.find_files, {silent=true, noremap=true})
 vim.keymap.set('n', '<leader>bb', builtin_pickers.buffers, {silent=true, noremap=true})
 vim.keymap.set('n', '<leader><leader><Space>', builtin_pickers.commands, {silent=true, noremap=true})
+vim.keymap.set('n', '<leader>oc', builtin_pickers.colorscheme, {silent=True, noremap=True})
+
