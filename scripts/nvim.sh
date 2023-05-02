0d71ef132920f00e3d9c857348b04a8d8e27ee81
El Kapuzo <el.kapuzo@mailbox.org>
2023-05-02 21:03:18 +0200

    fix(scripts): delete existing nvim installtion befor building

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -5,7 +5,9 @@ BUILD_DIR="$2/neovim"
 SUDO=$3
 
 PWD=$(pwd)
-NVIM_PY_PACKAGES="black flake8 flake8-bandit flake8-bugbear flake8-implicit-str-concat flake8-eradicate flake8-debugger falke8-commas flake8-broken-line isort jedi-language-server pynvim rope"
+
+$SUDO rm /usr/local/bin/nvim
+$SUDO rm -r /usr/local/share/nvim
 
 git clone --branch=$BRANCH --single-branch --depth 1 https://github.com/neovim/neovim $BUILD_DIR
 cd $BUILD_DIR
