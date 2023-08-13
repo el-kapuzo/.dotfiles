a237025b36e81928dd3a2407745ef63487cc4127
El Kapuzo <el.kapuzo@mailbox.org>
2023-08-13 07:40:21 +0200

    feat: configure external colorschemesa

---
+++ vim/lua/colorscheme-config.lua
@@ -0,0 +1,10 @@
+packadd = require 'utils'.packadd
+
+vim.opt.background = 'light'
+vim.opt.fillchars:append("vert:│")
+vim.opt.termguicolors = true
+
+packadd('spectrum-nvim')
+packadd('carbon-nvim')
+
+vim.cmd('colorscheme spectrum')
