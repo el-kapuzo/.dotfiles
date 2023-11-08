206c771ec9b4e22a06165fd80ff81bb83dddb384
El Kapuzo <el.kapuzo@mailbox.org>
2023-11-08 21:52:38 +0100

    refactor(vim): define key-maps in lua instead of vimscript

--- vim/lua/utils.lua
+++ vim/lua/utils.lua
@@ -1,7 +1,15 @@
 local utils = {}
 
-function utils.packadd(package) 
+function utils.packadd(package)
     vim.cmd("packadd! " .. package)
+end
+
+function utils.nnoremap(lhs, rhs)
+    vim.keymap.set("n", lhs, rhs, { noremap = true, silent = true })
+end
+
+function utils.vnoremap(lhs, rhs)
+    vim.keymap.set("v", lhs, rhs, { noremap = true, silent = true })
 end
 
 return utils
