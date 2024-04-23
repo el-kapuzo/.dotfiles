3eed57b8b07bc7e7bb2d47c785ca04168a389bbb
daniel pohl <daniel.pohl@up2parts.com>
2024-04-23 06:13:17 +0000

    feat(vim): use IntelliJ like autocomplete keybindings

--- vim/lua/completion.lua
+++ vim/lua/completion.lua
@@ -25,7 +25,17 @@ cmp.setup({
         { name = "buffer" }
     }),
     mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping.confirm(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                local entry = cmp.get_selected_entry()
+                if not entry then
+                    cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+                end
+                cmp.confirm()
+            else
+                fallback()
+            end
+        end)
     }),
     -- window = {
     --     completion = cmp.config.window.bordered(),
