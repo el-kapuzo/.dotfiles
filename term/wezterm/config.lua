dfa92abdaaf4d6a948b90cde4c3ecce3ba091550
da-pohl <daniel.pohl@openpack.com>
2024-07-09 18:00:23 +0200

    chore(term): refactor cursor in wezterm config

--- term/wezterm/config.lua
+++ term/wezterm/config.lua
@@ -1,19 +1,13 @@
 local wezterm = require 'wezterm'
-local key_tables = require 'key_tables'
-local key_mapping = require 'key_mapping'
-local colors = require 'colors'
-local windows = require 'windows'
-local font = require 'font'
+
+
+local config_modules = {
+    'key_tables', "key_mapping", "font", "colors", "windows", "cursor"
+}
 
 local config = wezterm.config_builder()
-
-key_tables.apply_to_config(config)
-key_mapping.apply_to_config(config)
-colors.apply_to_config(config)
-windows.apply_to_config(config)
-font.apply_to_config(config)
-
--- Cursor
-config.force_reverse_video_cursor = true
+for _, module in ipairs(config_modules) do
+    require(module).apply_to_config(config)
+end
 
 return config
---
+++ term/wezterm/cursor.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.apply_to_config = function(config)
+    config.force_reverse_video_cursor = true
+end
+
+return M
